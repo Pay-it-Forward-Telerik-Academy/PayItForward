53ab7728f51f1cf61a3c8978281a79742b559765
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-22 20:52:04 +0200

    Added predefined categories in Seed method

--- Source/Data/Startkicker.Data/Migrations/Configuration.cs
+++ Source/Data/Startkicker.Data/Migrations/Configuration.cs
@@ -3,7 +3,7 @@ namespace PayItForward.Data.Migrations
     using System.Data.Entity.Migrations;
 
     using PayItForward.Data;
-
+    using Models;
     public sealed class Configuration : DbMigrationsConfiguration<PayItForwardDbContext>
     {
         public Configuration()
@@ -14,7 +14,16 @@ namespace PayItForward.Data.Migrations
 
         protected override void Seed(PayItForwardDbContext context)
         {
-
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Medical" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Volunteer" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Emergencies" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Education" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Memorials" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Sports" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Animals" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Business" });
+            context.Categories.Add(new Category { IsRemoved = false, Name = "Other" });
+            context.SaveChanges();
         }
     }
 }
