01e9a071f9902a767b25f4abaae6e1c233569fab
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2016-01-25 10:55:55 +0200

    Hide admin panel from users in nav bar, after create story now redirect to details page, and redesign categories link on the home page

--- Source/PayItForward.WebForms/Site.Master.designer.cs
+++ Source/PayItForward.WebForms/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace PayItForward.WebForms {
         protected global::System.Web.UI.HtmlControls.HtmlForm multipartrequest;
         
         /// <summary>
+        /// admin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor admin;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
